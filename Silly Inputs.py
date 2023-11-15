40bc7264a5714d5138a9f8adee8c917c70d50120
Aiden B <133374942+Aiden-D-B@users.noreply.github.com>
2023-11-15 18:34:46 +0000

    Add files via upload

---
+++ Silly Inputs.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number between 1 and 10\n")) 
+calc = number+5 
+print(str(number)+" + 5 = "+str(calc)) 
+if number > 10: 
+    print("This number is not between 1-10")
+elif number < 1:
+    print("This number is not between 1-10")
